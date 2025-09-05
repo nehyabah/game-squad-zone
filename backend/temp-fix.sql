91ae63353dba4ae11bea283e24e280ef3f6b52f9
Nehemiah Abah Miagen <nabah@miagen.com>
2025-09-05 12:24:35 +0100

    fix 4

---
+++ backend/temp-fix.sql
@@ -0,0 +1,3 @@
+﻿UPDATE "User" SET "authProvider" = 'okta' WHERE "authProvider" IS NULL OR "authProvider" = '';
+ALTER TABLE "User" ALTER COLUMN "authProvider" TYPE text USING "authProvider"::text;
+ALTER TABLE "User" ALTER COLUMN "authProvider" SET DEFAULT 'okta';
